dd30738d7ba6cea4903e7bb10cded6882a7e2e7f
ave <veselya95@gmail.com>
2024-12-07 17:55:15 +0100

    Využití OrderItem

--- OrderAggregator/Services/OrderSender.cs
+++ OrderAggregator/Services/OrderSender.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using OrderAggregator.Models;
 using OrderAggregator.Services.Interfaces;
 
 namespace OrderAggregator.Services;
@@ -9,19 +10,17 @@ public class OrderSender(ILogger<OrderSender> logger, IServiceProvider servicePr
     {
         WriteIndented = true
     };
-    
+
     public async Task SendOrdersAsync()
     {
         using var scope = serviceProvider.CreateScope();
         var orderManager = scope.ServiceProvider.GetRequiredService<IOrderManager>();
         var orders = await orderManager.GetAggregatedOrdersAsync();
 
-        var jsonObject = orders.Select(item => new
-        {
-            productId = item.Key,
-            quantity = item.Value
-        }).ToList();
-            
+        var jsonObject = orders.Select(item
+                => new OrderItem(item.Key, item.Value))
+            .ToList();
+
         var json = JsonSerializer.Serialize(jsonObject, JsonWriteOptions);
 
         logger.LogInformation("{json}", json);
