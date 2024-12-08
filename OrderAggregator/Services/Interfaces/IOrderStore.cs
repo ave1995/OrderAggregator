5057a0f61b61a8af53b7723eec4e4bd2e3ad3870
ave <veselya95@gmail.com>
2024-12-08 15:14:37 +0100

    Rozšíření komentáře

--- OrderAggregator/Services/Interfaces/IOrderStore.cs
+++ OrderAggregator/Services/Interfaces/IOrderStore.cs
@@ -10,5 +10,6 @@ public interface IOrderStore
     Task<IImmutableList<OrderItem>> GetCurrentOrdersAsync();
     
     //Napadlo mě přidat tuhle metodu kvůli tomu, že by někdo chtěl dostávat jen aktuální data
+    //Například bych jednou za 10 min předal balík změn, vyčistil pamět a začal zase sbírat nové objednávky
     Task<IImmutableList<OrderItem>> ClearOrdersAndGetSnapshotAsync();
 }
